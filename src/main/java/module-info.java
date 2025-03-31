c1b580132d40eda623a130b6d75a318e4a742707
AU <NiceApplication1new@gmail.com>
2025-03-31 19:34:53 +1100

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module GraphingCalculator {
-    requires javafx.base;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    requires javafx.controls;
-
-    exports dev.StylishNerds.GraphingCalculator;
-    opens dev.StylishNerds.GraphingCalculator to javafx.fxml;
-}
